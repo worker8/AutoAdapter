39c7ce5ca4670bf114425feb2b0b1567dfffee93
Tan Jun Rong <tanjunrong@gmail.com>
2020-09-06 16:31:53 +0900

    add stable id and no stable id

--- app/src/main/java/com/worker8/autoadapter/SimpleActivity.kt
+++ app/src/main/java/com/worker8/autoadapter/SimpleActivity.kt
@@ -5,23 +5,35 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView.NO_ID
 import com.worker8.auto.adapter.library.AutoAdapter
+import com.worker8.auto.adapter.library.AutoData
 import com.worker8.auto.adapter.library.BaseRow
 import com.worker8.autoadapter.data.NormalAutoData
 import kotlinx.android.synthetic.main.activity_simple.*
 import kotlinx.android.synthetic.main.normal_row.view.*
 
 class SimpleActivity : AppCompatActivity() {
+    companion object {
+        val HAS_STABLE_ID = "HAS_STABLE_ID"
+    }
+
+    private val hasStableId by lazy {
+        intent.getBooleanExtra(HAS_STABLE_ID, false)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
-        val adapter = AutoAdapter(hasStableIds = false)
+        val adapter = AutoAdapter(hasStableIds = hasStableId)
         recyclerView.adapter = adapter
         val ROW_COUNT = 100
-        val list = mutableListOf()
-
-        list.add(
-            NormalRow(NormalAutoData(NO_ID, "title #${counter}", "desc #${counter++}"))
-        )
+        val list = mutableListOf<BaseRow<out AutoData>>()
+        for (i in 0 until ROW_COUNT) {
+            if (hasStableId) {
+                list.add(NormalRow(NormalAutoData(i.toLong(), "title #${i}", "desc #${i}")))
+            } else {
+                list.add(NormalRow(NormalAutoData(NO_ID, "title #${i}", "desc #${i}")))
+            }
+        }
         adapter.submitList(list)
     }
 }
--- app/src/main/java/com/worker8/autoadapter/TopActivity.kt
+++ app/src/main/java/com/worker8/autoadapter/TopActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.worker8.auto.adapter.library.AutoAdapter
+import com.worker8.autoadapter.SimpleActivity.Companion.HAS_STABLE_ID
 import com.worker8.autoadapter.data.StringData
 import com.worker8.autoadapter.rows.SimpleRow
 import kotlinx.android.synthetic.main.activity_top.*
@@ -16,8 +17,19 @@ class TopActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
 
         val list = listOf(
-            SimpleRow(StringData(text = "SimpleActivity")) {
-                startActivity(Intent(this@TopActivity, SimpleActivity::class.java))
+            SimpleRow(StringData(text = "SimpleActivity - no stable id")) {
+                startActivity(
+                    Intent(this@TopActivity, SimpleActivity::class.java).putExtra(
+                        HAS_STABLE_ID, false
+                    )
+                )
+            },
+            SimpleRow(StringData(text = "SimpleActivity - has stable id")) {
+                startActivity(
+                    Intent(this@TopActivity, SimpleActivity::class.java).putExtra(
+                        HAS_STABLE_ID, true
+                    )
+                )
             },
             SimpleRow(StringData(text = "BasicListActivity")) {
                 startActivity(Intent(this@TopActivity, BasicListActivity::class.java))
